f953e419a51432d5daa86cbdf71af7cad28eacb7
Akinpelu Oluwatosin <fortifiedtosin@gmail.com>
2025-08-23 19:59:58 +0100

    Add files via upload

---
+++ cycleCheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+	public class cycleCheck{
+	public static void main(String...args){
+
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("1: TO KNOW YOUR SAFE PERIOD");
+
+	System.out.println("2: TO KNOW YOUR OVULATION DATES");
+
+	System.out.println("3: TO KNOW YOUR FLOW DATE");
+
+	System.out.println("4: TO EXIT");
+	 
+	System.out.print("SELECT AN OPTION: ");
+}
+
+}
