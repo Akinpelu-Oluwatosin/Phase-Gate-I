088479c589c4d591c7240d3e31756719b5b87081
Akinpelu Oluwatosin <fortifiedtosin@gmail.com>
2025-08-23 20:20:31 +0100

    Add files via upload
    updated a function to check safe period

--- cycleCheck.java
+++ cycleCheck.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 	public class cycleCheck{
+
 	public static void main(String...args){
 
 	Scanner input = new Scanner(System.in);
@@ -14,6 +15,35 @@ import java.util.Scanner;
 	System.out.println("4: TO EXIT");
 	 
 	System.out.print("SELECT AN OPTION: ");
+	
+	int userOption = input.nextInt();
+	
+	if(userOption == 1){
+
+	safePeriod(8);
+
 }
 
+
+	
+}
+
+	public static void safePeriod(int thedate){
+	
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("What day of your cycle are you?: ");
+	
+	int date = input.nextInt();
+	
+	int safeDate = 28;
+	
+	if(date < safeDate){
+
+	System.out.println("You are likely to get pregnant this period");
+}
+	else{
+	System.out.println("You're free to have unprotected sex");
+}
+}
 }
