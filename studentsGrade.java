0cc18880d7740e9d27b106e8ed528379897533a9
Akinpelu Oluwatosin <fortifiedtosin@gmail.com>
2025-09-01 06:12:46 +0100

    Add files via upload

--- studentsGrade.java
+++ studentsGrade.java
@@ -10,17 +10,19 @@ import java.util.Scanner;
 		int studentsAmount = input.nextInt();
 	
 		System.out.println("How many subjects?: ");
-		int studentsSubject = input.nextInt();
+		int studentsSubjectAmount = input.nextInt();
 
 		int[][] scoresAndSubjects = new int[studentsAmount][studentsSubject];	
-	
+		String[] studentNames = new String[studentsAmount];
+		System.out.println("Enter the student's name");
+
 		for(int students = 0; students <= studentsAmount; students++){
 		System.out.println("student" + (students + 1) + ":");
 } 
-		for(int index = 0; index <= studentsAmount.length; index++){
+		for(int subject = 0; subject <= studentsAmount.length; subject++){
 		System.out.println("Input the students subjects" + (students + 1) + ":" + studentsAmount[students]);
 		String studentsName = input.nextLine();
 }
-}
+}                  
 
 }
